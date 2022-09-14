d3b572a328e378db4dc5f9a0513f9bc0481bcba2
Srithulasis <112751897+Srithulasis@users.noreply.github.com>
2022-09-14 09:32:22 +0530

    Create odd or even.c

---
+++ odd or even.c
@@ -0,0 +1,15 @@
+    #include<stdio.h>
+    int main() {
+    	int number;
+    	printf("Enter any integer: ");
+    	scanf("%d",&number);
+    	if(number % 2 ==0)
+    	         printf("%d is even number.",number); else
+    	         printf("%d is odd number.",number);
+    	return 0;
+    }
+
+o/p
+
+Enter any integer: 5
+5 is odd number.
